250712a0102f2741ec062c2acfe1cfd116c4f7cc
MaykerHM <hydeki8@gmail.com>
2023-09-15 22:05:18 -0300

    feat: apply space template on mansory template

--- src/components/template/stories/ArchbaseSpaceTemplate.story.tsx
+++ src/components/template/stories/ArchbaseSpaceTemplate.story.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Input, Text } from '@mantine/core';
+import { Card, Input, Text } from '@mantine/core';
 import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { ArchbaseSpaceTemplate } from '../ArchbaseSpaceTemplate';
@@ -7,40 +7,38 @@ import { Actions } from '@components/buttons/ArchbaseActionButtons.story';
 
 export const ArchbaseSpaceTemplateExample = () => {
   return (
-    <Grid>
-      <Grid.Col span={12}>
-        <ArchbaseSpaceTemplate
-          title={'Pessoas'}
-          options={{
-            headerFlexGrow: 'right',
-          }}
-          headerLeft={
-            <Card>
-              <Text>Pesquisar</Text>
-              <Input miw={'300px'} />
-            </Card>
-          }
-          headerRight={
-            <ArchbaseActionButtons
-              actions={Actions}
-              options={{
-                largerBreakPoint: '800px',
-                smallerBreakPoint: '400px',
-                largerSpacing: '2rem',
-                smallerSpacing: '0.5rem',
-                largerButtonVariant: 'filled',
-                smallerButtonVariant: 'filled',
-                menuItemVariant: 'filled',
-                menuButtonVariant: 'filled',
-                menuButtonColor: 'blue.5',
-                menuItemApplyActionColor: true,
-                menuPosition: 'right',
-              }}
-            />
-          }
-        />
-      </Grid.Col>
-    </Grid>
+    <div style={{ width: '100%', height: 'calc(100vh - 50px)' }}>
+      <ArchbaseSpaceTemplate
+        title={'Pessoas'}
+        options={{
+          headerFlexGrow: 'right',
+        }}
+        headerLeft={
+          <Card>
+            <Text>Pesquisar</Text>
+            <Input miw={'300px'} />
+          </Card>
+        }
+        headerRight={
+          <ArchbaseActionButtons
+            actions={Actions}
+            options={{
+              largerBreakPoint: '800px',
+              smallerBreakPoint: '400px',
+              largerSpacing: '2rem',
+              smallerSpacing: '0.5rem',
+              largerButtonVariant: 'filled',
+              smallerButtonVariant: 'filled',
+              menuItemVariant: 'filled',
+              menuButtonVariant: 'filled',
+              menuButtonColor: 'blue.5',
+              menuItemApplyActionColor: true,
+              menuPosition: 'right',
+            }}
+          />
+        }
+      />
+    </div>
   );
 };
 
