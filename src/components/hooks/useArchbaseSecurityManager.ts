5df9ab8aa3f19f525a3647624d1ea60aa2163349
mayker <hydeki8@gmail.com>
2024-06-29 20:44:53 -0300

    refactor: deixar o erro somente na classe

--- src/components/hooks/useArchbaseSecurityManager.ts
+++ src/components/hooks/useArchbaseSecurityManager.ts
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react'
 import { ArchbaseSecurityManager } from '@components/security/ArchbaseSecurityManager'
 import { ArchbaseStore, useArchbaseStore } from './useArchbaseStore'
-import { processErrorMessage } from '@components/core'
 
 export type UseArchbaseSecurityManagerProps = {
   resourceName: string
   resourceDescription: string
-  onError?: (error, originError) => void
 }
 
 export type UseArchbaseSecurityManagerReturnType = {
@@ -14,49 +12,27 @@ export type UseArchbaseSecurityManagerReturnType = {
 }
 
 const buildSecurityManager = (store: ArchbaseStore, resourceName: string, resourceDescription: string) => {
-  if (store && store.existsValue(resourceName)){
+  if (store && store.existsValue(resourceName)) {
     return store.getValue(resourceName);
   }
-
   return new ArchbaseSecurityManager(resourceName, resourceDescription)
 }
 
 type UseArchbaseSecurityManagerState = {
   securityManager: ArchbaseSecurityManager | null
-  isError: boolean
-  error: any
 }
 
 export const useArchbaseSecurityManager = (
   props: UseArchbaseSecurityManagerProps
 ): UseArchbaseSecurityManagerReturnType => {
-  const { resourceName, resourceDescription, onError} = props
+  const { resourceName, resourceDescription } = props
   const store = useArchbaseStore('archbaseSecurityManagerStore')
   const [internalState, setInternalState] = useState<UseArchbaseSecurityManagerState>({
-    securityManager: buildSecurityManager(store, resourceName, resourceDescription),
-    isError: false,
-    error: '',
+    securityManager: buildSecurityManager(store, resourceName, resourceDescription)
   })
 
   useEffect(() => {
-    // try {
-      // const securityManager = buildSecurityManager(store, resourceName, resourceDescription)
-      store.setValue(resourceName, internalState.securityManager);
-    //   setInternalState((prev) => ({
-    //     ...prev,
-    //     securityManager,
-    //   }))
-    // } catch (error) {
-    //   const userError = processErrorMessage(error)
-    //   setInternalState((prev) => ({
-    //     ...prev,
-    //     isError: true,
-    //     error: userError
-    //   }))
-    //   if (onError) {
-    //     onError(userError, error)
-    //   }
-    // }
+    store.setValue(resourceName, internalState.securityManager);
   }, [resourceName])
 
   return { securityManager: internalState.securityManager }
--- src/components/security/ArchbaseSecurityManager.tsx
+++ src/components/security/ArchbaseSecurityManager.tsx
@@ -1,4 +1,4 @@
-import { ARCHBASE_IOC_API_TYPE, IOCContainer } from '@components/core';
+import { ARCHBASE_IOC_API_TYPE, IOCContainer, processErrorMessage } from '@components/core';
 import { ArchbaseResourceService } from './ArchbaseResourceService';
 import { ResourcePermissionsDto, SimpleActionDto, SimpleResourceDto } from './SecurityDomain';
 
@@ -13,29 +13,45 @@ export class ArchbaseSecurityManager implements ISecurityManager {
   protected actions: SimpleActionDto[]
   protected permissions: string[]
   protected alreadyApplied: boolean
+  protected error: string
 
   constructor(resourceName: string, resourceDescription: string) {
     this.resourceService = IOCContainer.getContainer().get<ArchbaseResourceService>(ARCHBASE_IOC_API_TYPE.Resource);
-    this.resource = {resourceName, resourceDescription}
+    this.resource = { resourceName, resourceDescription }
     this.alreadyApplied = false
     this.actions = []
+    this.permissions = []
+    this.error = ""
   }
 
   public registerAction(actionName: string, actionDescription: string) {
     if (!this.alreadyApplied) {
-      this.actions.push({actionName, actionDescription})
+      this.actions.push({ actionName, actionDescription })
     }
   }
 
   public async apply() {
     if (!this.alreadyApplied) {
-      const resourcePermissions: ResourcePermissionsDto = await this.resourceService.registerResource({resource: this.resource, actions: this.actions})
-      this.permissions = resourcePermissions.permissions;
-      this.alreadyApplied = true
+      try {
+        const resourcePermissions: ResourcePermissionsDto = await this.resourceService.registerResource({ resource: this.resource, actions: this.actions })
+        this.permissions = resourcePermissions.permissions;
+        this.alreadyApplied = true
+        this.error = ""
+      } catch (error) {
+        this.error = processErrorMessage(error)
+      }
     }
   }
 
   public hasPermission(actionName: string) {
-    this.permissions.includes(actionName)
+    return this.permissions.includes(actionName)
+  }
+
+  public isError() {
+    return !!this.error
+  }
+
+  public getError() {
+    return this.error
   }
 }
