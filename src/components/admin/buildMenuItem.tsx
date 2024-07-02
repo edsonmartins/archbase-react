e1eea20143366e80e439994a71590bc2baec9e4f
mayker <mayker.hydeki@webpublico.com.br>
2024-07-02 16:57:07 -0300

    fix: adicionar o active nos itens do menu que são avulsos também

--- src/components/admin/buildMenuItem.tsx
+++ src/components/admin/buildMenuItem.tsx
@@ -56,6 +56,7 @@ export function buildMenuItem(theme, collapsed, onMenuItemClick, item, index, ic
 					disabled={typeof item.disabled === 'function' ? item.disabled() : item.disabled}
 					onClick={() => onMenuItemClick(item)}
 					icon={item.icon}
+					active={highlightActiveMenuItem && item.link === currentPathName}
 				>
 					{collapsed ? '' : `${i18next.t(item.label)}`}
 				</MenuItem>
